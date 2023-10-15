8796d0c01393beedde74be5b60571c12d31ad6d0
CheGlenn <118393138+CheGlenn@users.noreply.github.com>
2023-10-14 20:42:53 -0500

    Update AACCategory.java
    added getTexts method

--- src/AACCategory.java
+++ src/AACCategory.java
@@ -38,20 +38,26 @@ public class AACCategory<K,V>{
 
   /*
    * Adds the mapping of the imageLoc to the text to the category
+   * pre: String imageLocation, String text
+   * post:
    */
   public void addItem(String imageLoc, String text){
     this.categories.set(imageLoc, text);
-  }
+  } //addItem()
 
   /*
    * Returns the name of the category
+   * pre: 
+   * post: returns String category name
    */
   public String getCategory(){
     return this.name;
-  }
+  } //getCategory()
 
   /*
    * Returns an array of all the images in the category
+   * pre: 
+   * post: returns String[] of allImages
    */
   public String[] getImages(){
  
@@ -60,20 +66,37 @@ public class AACCategory<K,V>{
  
     return allImages; // return string array
     
-  } 
+  } //getImages()
+
+  /*
+   * Returns an array of all images in the category
+   * pre: 
+   * post: returns String[] of allTexts
+   */
+
+   public String[] getTexts(){
+    List<String> txts = this.categories.returnValues(); //list to store values 
+    String[] allTexts = txts.toArray(new String[txts.size()]); //save list to String array
+
+    return allTexts; //return string array
+   } //getTexts()
 
   /*
    * Returns the text associated with the given image loc in this category
+   * pre: String imageLocation
+   * post: returns String value of assoc key
    */
   public String getText(String imageLoc) throws KeyNotFoundException{
-    return this.categories.get(imageLoc);
-  }
+    return this.categories.get(imageLoc); //returns value of assoc key
+  } //getText()
 
   /*
    * Determines if the provided images is stored in the category
+   * pre: String imageLocation
+   * post: returns Bool ife key imageLoc is found in AA
    */
   public Boolean hasImage(String imageLoc){
     return this.categories.hasKey(imageLoc);
-  }
+  }//hasImage()
   
 }
